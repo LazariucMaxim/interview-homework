42b5353b4a0584dcf83222e9d98aacf213314f05
Lazariuc Maxim <lazariucmaxim@yandex.ru>
2024-06-25 16:33:15 +0300

    Add type annotations in models

--- models/stack.py
+++ models/stack.py
@@ -1,22 +1,27 @@
+from typing import Any
+
+
 class Stack:
-    def __init__(self):
+    _elements: list
+
+    def __init__(self) -> None:
         self._elements = []
 
-    def push(self, element):
+    def push(self, element: Any) -> None:
         self._elements.append(element)
 
-    def size(self):
+    def size(self) -> int:
         return len(self._elements)
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return not self.size()
 
-    def pop(self):
+    def pop(self) -> Any:
         return self._elements.pop()
 
-    def peek(self):
+    def peek(self) -> Any:
         if not self.is_empty():
             return self._elements[-1]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'stack({", ".join(map(str, self._elements))})'
